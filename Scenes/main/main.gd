d6e57d8c9500f04b1de7475330fb4d87e5d5843f
DawsonEpp <daws12@me.com>
2026-03-31 19:54:31 -0600

    added the start wave button

--- Scenes/main/main.gd
+++ Scenes/main/main.gd
@@ -2,8 +2,16 @@ extends Node2D
 
 func _ready():
 	Globals.mainNode = self
-	#Globals.bubble_cursor = $UI/HUD/BubbleCursor
 	var selectedMapScene := load(Data.maps[Globals.selected_map]["scene"])
 	var map = selectedMapScene.instantiate()
 	map.map_type = Globals.selected_map
 	add_child(map)
+
+	# Honour bubble cursor setting chosen on main menu when implemented
+	var bc = $UI/BubbleCursor
+	if Globals.bubble_cursor_enabled:
+		bc.visible = true
+		Globals.bubble_cursor = bc
+	else:
+		bc.visible = false
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
