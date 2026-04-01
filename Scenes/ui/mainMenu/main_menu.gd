a0e3aacd026beec06960f168702bd1768dcdf2ef
DawsonEpp <daws12@me.com>
2026-03-31 20:07:30 -0600

    added a checkbox tp tje main menu to enable and disable bubble cursor

--- Scenes/ui/mainMenu/main_menu.gd
+++ Scenes/ui/mainMenu/main_menu.gd
@@ -2,12 +2,19 @@ extends Control
 
 var mapSelectContainer : PanelContainer
 
+func _ready():
+	# Reflect the current state of the toggle on load
+	%BubbleCursorCheck.button_pressed = Globals.bubble_cursor_enabled
+
 func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _on_start_button_pressed():
 	if not mapSelectContainer:
-		var mscScene := preload("res://Scenes/ui/mainMenu/select_map_container.tscn")
-		var msc := mscScene.instantiate()
+		var mscScene = preload("res://Scenes/ui/mainMenu/select_map_container.tscn")
+		var msc= mscScene.instantiate()
 		mapSelectContainer = msc
 		add_child(msc)
+
+func _on_bubble_cursor_check_toggled(toggled_on: bool):
+	Globals.bubble_cursor_enabled = toggled_on
